5edad6335b9b9625cd5afb82a78d7de43d257e7d
Gabrielle Milare Ferreira <gabimilare25@gmail.com>
2023-05-30 07:57:02 -0300

    login

--- js/loginScript.js
+++
@@ -1,8 +0,0 @@
-function login(){
-    let login = document.getElementById("login").value;
-    let senha = document.getElementById("senha").value;
-
-    if (login == senha){
-        window.location.href = "restrito.html";
-    }
-};
